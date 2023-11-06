0f7510e86d217e6d905a01e13db9c4af225a2933
arth <arth@globaltllc.com>
2023-11-06 13:00:11 +0800

    for deployment - test 1

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,16 @@
+import express from "express";
+import path from "path";
 import app from "./app";
 import env from "./lib/env";
-app.listen(env.PORT, () => {
-  console.log("Server running on port: " + env.PORT);
-});
+
+const __dirname1 = path.resolve();
+if (env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.listen(env.PORT, () => {
+    console.log("Server running on port: " + env.PORT);
+  });
+}
